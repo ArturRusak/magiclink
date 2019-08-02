e5a5bb7037e9c9581d4d633f6e0809b6193b222e
artur.rusak <artur.rusak@instinctools.ru>
2019-08-02 14:46:26 +0300

    setting of fetch data from server

--- client/src/components/screens/link/List.js
+++ client/src/components/screens/link/List.js
@@ -3,14 +3,29 @@ import { Table } from "baseui/table";
 import { Block } from "baseui/block";
 
 class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: "",
+      listLinks: []
+    };
+  }
+
+  componentDidMount() {
+    const request = new Request("http://localhost:3001/links");
+    fetch(request)
+      .then(response => response.json())
+      .then(data => this.setState({ status: data.status, listLinks: data.data }));
+  }
   render() {
-    const titles = ["Link", "Hash Link"];
-    const data = [["test", 1], ["test", 2]];
+    const { listLinks } = this.state;
+    console.log(listLinks);
+    const titles = ["_id", "id", "Link", "Hash Link"];
     return (
       <React.Fragment>
         <h1>Magic Links</h1>
         <Block margin={"1.5em 0"}>
-          <Table columns={titles} data={data}/>
+          <Table columns={titles} data={listLinks.map((item) => Object.values(item))}/>
         </Block>
       </React.Fragment>
     );
