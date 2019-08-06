c4099493d7cbc09fa4a20d38431a780e226ace81
artur.rusak <artur.rusak@instinctools.ru>
2019-08-06 11:54:45 +0300

    rename

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,6 @@
-export { default as Header } from "./common/header/Header";
-export { default as Footer } from "./common/footer/Footer";
-export { default as ContentBody } from "./common/contentBody/ContentBody";
+export { default as Header } from "./common/Header/Header";
+export { default as Footer } from "./common/Footer/Footer";
+export { default as ContentBody } from "./common/ContentBody/ContentBody";
 
 /*Screens*/
-export { default as LinksList } from "./screens/link/List";
+export { default as LinksList } from "./screens/Link/List";
