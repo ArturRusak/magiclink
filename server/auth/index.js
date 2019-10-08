99d97a53e072d521e56d2d16191a04e0fe4f460c
arturrusak <arturrusakk@gmail.com>
2019-10-09 00:01:05 +0300

    investigation

--- server/auth/index.js
+++ server/auth/index.js
@@ -3,12 +3,15 @@ const bcrypt = require("bcrypt");
 const {usersModel} = require("../DAO");
 
 module.exports = function (passport) {
-
   passport.use(
     new LocalStrategy((username, password, done) => {
       // Match user
       console.log(username, password, "--------");
-      done(null, {username, password})
+      done(null, {username, password;
+    })
+      ;
+      ;
+      ;
       /*  User.findOne({
         email: email
       }).then(user => {
@@ -30,11 +33,12 @@ module.exports = function (passport) {
   );
 
   passport.serializeUser(function ({username}, done) {
-    console.log(1111111, username)
+    console.log(1111111, username);
+    ;
     done(null, {test: username});
   });
 
   passport.deserializeUser(function (username, done) {
-    done(null, '1');
+    done(null, "1");
   });
 };
