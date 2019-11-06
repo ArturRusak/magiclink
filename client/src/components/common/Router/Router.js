db7e422d3be9d8719cdf110727135013cf11363d
arturrusak <arturrusakk@gmail.com>
2019-11-07 00:31:54 +0300

    add registration page

--- client/src/components/common/Router/Router.js
+++ client/src/components/common/Router/Router.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../index";
 
-§import { Home, LinkInfo, LinksList, Login, Registration } from "../../index";
+import { Home, LinkInfo, LinksList, Login, Registration } from "../../index";
 
 import { styled } from "baseui";
 import { Switch, Route, Redirect } from "react-router-dom";
