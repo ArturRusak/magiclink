835098454e7dfca95388af7d3cc639a5edf4dc57
arturrusak <arturrusakk@gmail.com>
2019-11-12 00:46:08 +0300

    REFACTORING

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const bcrypt = require("bcrypt");
 const ObjectId = require("mongodb").ObjectId;
 const { usersModel } = require("../DAO");
 
@@ -45,7 +46,23 @@ function findUserByNickName(nick) {
  * @returns {Promise<any>}
  */
 function saveUser(user) {
-  return usersModel.saveUser(user);
+  let {password} = user;
+  const {username} = user;
+
+  return usersModel
+    .checkUser({username})
+    .then(foundUser => {
+      if (!foundUser) {
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = bcrypt.hashSync(password, salt);
+
+        return usersModel.saveUser({...user, password: hashPassword});
+      }
+      return Promise.reject("The user already exists!");
+    })
+    .catch(error => {
+      return Promise.reject(error);
+    });
 }
 
 module.exports = {
